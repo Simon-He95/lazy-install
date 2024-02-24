b0d4eb5df86fec544332a9029101374315ed1ec2
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-02-24 13:30:36 +0800

    chore: update deps

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,21 @@ import type { Disposable, ExtensionContext, Terminal } from 'vscode'
 import { createInstallCodeLensProvider, detectModule, getPnpmWorkspace } from './utils'
 
 export const pnpmWorkspace = getPnpmWorkspace()
+let terminal: Terminal
 export async function activate(context: ExtensionContext) {
   const disposes: Disposable[] = []
   detectModule()
   disposes.push(createInstallCodeLensProvider())
-  disposes.push(addEventListener('text-change', () => detectModule()))
-  disposes.push(addEventListener('activeText-change', () => detectModule()))
-
-  let terminal: Terminal
-
+  disposes.push(addEventListener('text-change', detectModule))
+  disposes.push(addEventListener('activeText-change', detectModule))
   disposes.push(registerCommand('lazy-install.install', (_, name: string) => {
     // 考虑复用terminal
     if (!terminal)
       terminal = createTerminal('lazy-install')
     const installWay = getConfiguration('lazy-install.way')
-    const currentFileUrl = getCurrentFileUrl()! as string
+    const currentFileUrl = getCurrentFileUrl()!
+    if (!currentFileUrl)
+      return
     const isInWorkspace = pnpmWorkspace
       ? pnpmWorkspace.some((w: string) => currentFileUrl.indexOf(w))
       : false
@@ -25,7 +25,7 @@ export async function activate(context: ExtensionContext) {
     terminal.processId.then(() => {
       setTimeout(() => {
         // 考虑在monorepo子仓下安装, 需要补充 -w
-        terminal.sendText(`${installWay} ${name} ${isInWorkspace ? '-w' : ''}`)
+        terminal?.sendText(`${installWay} ${name} ${isInWorkspace ? '-w' : ''}`)
       }, 800)
     })
   }))
@@ -34,5 +34,5 @@ export async function activate(context: ExtensionContext) {
 }
 
 export function deactivate() {
-
+  terminal?.dispose()
 }
